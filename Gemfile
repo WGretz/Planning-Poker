dcd807deb42db936b7a58d1ddc404c4563bd358c
Will Gretz <Will@WillGretz.com>
2012-01-04 18:21:59 -0800

    sinatra template

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source :rubygems
+
+gem "sinatra"
+gem 'sinatra-backbone', :require => 'sinatra/backbone'
+gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
+gem 'sequel'
+gem 'sqlite3-ruby', :require => 'sqlite3'
---
+++ application.rb
@@ -0,0 +1,34 @@
+class MyApp < Sinatra::Base
+  
+  set :root, File.dirname(__FILE__)
+  register Sinatra::AssetPack
+  
+  DB = Sequel.sqlite('cube.db')
+  
+  assets {
+    serve '/js',     from: 'app/js'        # Optional
+    serve '/css',    from: 'app/css'       # Optional
+    serve '/images', from: 'app/images'    # Optional
+    
+    js :app, '/js/app.js', [
+      '/jst.js',
+      '/js/**/*.js',
+      '/js/script.js'
+    ]
+
+    css :application, '/css/application.css', [
+      '/css/screen.css'
+    ]
+
+    js_compression  :jsmin      # Optional
+    css_compression :sass       # Optional
+  }
+  
+  register Sinatra::JstPages
+  serve_jst '/jst.js'
+  
+  get '/' do
+    erb :index
+  end
+  
+end22
